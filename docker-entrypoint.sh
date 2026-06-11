1a618f50b7e4b13280c3894dcc808c3d119a5356
Yvette <1402738213@qq.com>
2026-06-11 15:04:54 +0800

    Change IP Address

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 # 默认值（与.env.production一致）
-VITE_GRAPHQL_URI="${VITE_GRAPHQL_URI:-http://54.224.156.11:8082/graphql}"
-VITE_SERVER_URI="${VITE_SERVER_URI:-http://54.224.156.11:8082}"
+VITE_GRAPHQL_URI="${VITE_GRAPHQL_URI:-http://50.17.76.198:8082/graphql}"
+VITE_SERVER_URI="${VITE_SERVER_URI:-http://50.17.76.198.11:8082}"
 # 替换JS文件中的占位符为实际运行时环境变量
 find /usr/share/nginx/html/assets -name '*.js' -exec sed -i \
 "s|__VITE_GRAPHQL_URI_PLACEHOLDER__|${VITE_GRAPHQL_URI}|g" {} +
